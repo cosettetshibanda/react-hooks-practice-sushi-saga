9e9d411bc177ac452398e9c575ec3f246b3b5408
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-03-26 23:27:58 -0600

    commit tree 8f7e03cb31cb611506330a2faf6f5f8ca62d256c

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi.js"
 
-function SushiContainer(props) {
+function SushiContainer({sushis, eatSushi}) {
+  const [sushiIndex, setSushiIndex] = useState(0)
+
+  function handleClick(){
+    setSushiIndex(sushiIndex + 4)
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushis.slice(sushiIndex, sushiIndex + 4).map(sushi => <Sushi eatSushi={eatSushi} sushi={sushi} key={sushi.id}/>)} 
+      <MoreButton handleClick={handleClick} />
     </div>
   );
 }
